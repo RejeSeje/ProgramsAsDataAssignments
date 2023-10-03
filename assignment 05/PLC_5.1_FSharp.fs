f9d4011181470826ce808e37e751e6a51be37d56
Andreas <andreasmwulff@gmail.com>
2023-10-03 11:18:29 +0200

    PLC 5.1 FSharp

---
+++ assignment 05/PLC_5.1_FSharp.fs
@@ -0,0 +1,7 @@
+﻿let rec merge x : int list =
+    match x with
+    | [],[] -> []
+    | x :: xs, [] -> x :: merge (xs, [])
+    | [], y :: ys -> y :: merge ([], ys)
+    | x :: xs, y :: ys when x > y -> y :: merge (x :: xs, ys)
+    | x :: xs, y :: ys when x <= y -> x :: merge (xs, y :: ys);;
