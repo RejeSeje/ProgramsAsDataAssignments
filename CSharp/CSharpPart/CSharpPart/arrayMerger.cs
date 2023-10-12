97096e04200ccb74a4937d101cf5bc928fbf9679
Viggo Starcke <vist@itu.dk>
2023-10-12 12:02:05 +0200

    PLC 6.5

---
+++ CSharp/CSharpPart/CSharpPart/arrayMerger.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpPart;
+
+public class arrayMerger
+{
+    static int[] merge(int[] xs, int[] ys)
+    {
+        return xs.Concat(ys).OrderBy(x => x).ToArray();
+    }
+    
+    int[] xs = { 3, 5, 12 };
+    int[] ys = { 2, 3, 4, 7 };
+
+    merge(xs, ys);
+}
