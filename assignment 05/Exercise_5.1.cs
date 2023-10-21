e8dcb18751fdb2fcc5b9852c3d271e106a210134
Viggo Starcke <vist@itu.dk>
2023-10-21 23:23:12 +0200

    5.1 C# corrected

--- assignment 05/Exercise_5.1.cs
+++ assignment 05/Exercise_5.1.cs
@@ -1,7 +1,45 @@
 public static void Main(string[] args)
 {
-    static int[] merge(int[] xs, int[] ys)
+    /* Old solution, doesn't follow intention of exercise */
+    // static int[] merge(int[] xs, int[] ys)
+    // {
+    //     return xs.Concat(ys).OrderBy(x => x).ToArray();
+    // }
+    
+    static int[] Merge(int[] xs, int[] ys)
     {
-        return xs.Concat(ys).OrderBy(x => x).ToArray();
+        int[] mergedArray = new int[xs.Length + ys.Length];
+        int i = 0, j = 0, k = 0;
+            
+        while (i < xs.Length && j < ys.Length)
+        {
+            if (xs[i] < ys[j])
+            {
+                mergedArray[k] = xs[i];
+                i++;
+            }
+            else
+            {
+                mergedArray[k] = ys[j];
+                j++;
+            }
+            k++;
+        }
+            
+        while (i < xs.Length)
+        {
+            mergedArray[k] = xs[i];
+            i++;
+            k++;
+        }
+            
+        while (j < ys.Length)
+        {
+            mergedArray[k] = ys[j];
+            j++;
+            k++;
+        }
+            
+        return mergedArray;
     }
 }
